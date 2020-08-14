8a163c5b1417c73756770b35529b6f3444ebb1be
sergejs1606@gmail.com <Armad1606>
2020-08-14 17:56:16 +0300

    Init

---
+++ Web_API_Exercise1/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web_API_Exercise1.Models;
+using Web_API_Exercise1.Services;
+
+namespace Web_API_Exercise1.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+
+        [HttpGet("api/[controller]")]
+        public List<Contact> GetAll()
+        {///api/contact
+            return contactRepository.GetAllContacts();
+        }//[{"id":1,"name":"Glenn Block"},{"id":2,"name":"Dan Roth"}]
+
+        [HttpGet("api/[controller]/{id}")]
+        public Contact Get(int id)
+        {///api/contact/2
+            return contactRepository.GetContactById(id);
+
+        }//{"id":2,"name":"Dan Roth"}
+
+        [HttpPost("api/[controller]/{name}")]
+        public bool Post(string name)
+        {
+            return contactRepository.SaveContact(name);
+
+        }
+
+        [HttpPut("api/[controller]/{id}/{value}")]
+        public bool Put(int id, string value)
+        {
+            return contactRepository.ChangeContact(id, value);
+
+        }
+
+    }
+}
---
+++ Web_API_Exercise1/Services/ContactRepository.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Web_API_Exercise1.Models;
+
+namespace Web_API_Exercise1.Services
+{
+	public class ContactRepository
+	{
+		private const string databaseFileName = "database.json";
+
+		public List<Contact> GetAllContacts()
+        {
+			string jsonData = System.IO.File.ReadAllText(databaseFileName);
+			return string.IsNullOrEmpty(jsonData) 
+				? new List<Contact>() 
+				: JsonConvert.DeserializeObject<List<Contact>>(jsonData);
+		}
+
+		public Contact GetContactById(int id)
+		{
+			List<Contact> contacsList = GetAllContacts();
+
+			foreach (Contact c in contacsList){
+				if (c.Id == id) return c;
+            }
+
+			return null;
+		}
+
+		public bool SaveContact(string name)
+        {
+			List<Contact> contacsList = GetAllContacts();
+			contacsList.Add(new Contact
+			{
+				Id = contacsList.Count + 1,
+				Name = name
+
+			}
+			);
+
+			//JsonSerializer serializer = new JsonSerializer();
+			//serialize object directly into file stream
+			string json = JsonConvert.SerializeObject(contacsList);
+
+			//write string to file
+			System.IO.File.WriteAllText(databaseFileName, json);
+
+
+			return true;
+
+		}
+
+		public bool ChangeContact(int id, string name)
+		{
+			List<Contact> contacsList = GetAllContacts();
+			foreach (Contact c in contacsList)
+			{
+				if (c.Id == id)
+				{
+					c.Name = name;
+					string json = JsonConvert.SerializeObject(contacsList);
+
+					//write string to file
+					System.IO.File.WriteAllText(databaseFileName, json);
+
+
+					return true;
+
+				}
+			}
+
+			//JsonSerializer serializer = new JsonSerializer();
+			//serialize object directly into file stream
+
+			return false;
+		}
+	}
+
+}
